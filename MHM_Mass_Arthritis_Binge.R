29e0e9798ce69e6b940a61203aae7a50c1cf4fe4
ndantuluri429 <139188813+ndantuluri429@users.noreply.github.com>
2023-07-13 13:30:04 -0400

    Health Outcomes Measures (MA) w/ specific clinic location & hyperlinked popups

--- MHM_Mass_Arthritis_Binge.R
+++ MHM_Mass_Arthritis_Binge.R
@@ -1,4 +1,5 @@
-# MHM Map + Crude Prevalence of arthritis
+# MHM Map + Crude Prevalence 
+# w/ clinic locations & hyperlinks to each one
 
 library(tigris)
 library(dplyr)
@@ -19,6 +20,10 @@ eva$GEOID <- as.double(eva$GEOID)
 mhm_filtered <- subset(MHM_Data, country == "United States")
 mhm_data_filtered <- mhm_filtered[complete.cases(mhm_filtered$latitude, mhm_filtered$longitude), ]
 mhm_filtered_mass <- subset(mhm_data_filtered, state_province %in% c("Massachusetts", "Ma"))
+
+
+
+####### ARTHRITIS #############################################################
 
 
 mass <- census_tract_data[census_tract_data$StateAbbr == "MA", ]
@@ -70,80 +75,56 @@ leaflet() %>%
             position = "bottomright",
             title="Crude Prevalence Arthritis")
 
+####### BINGE DRINKING ########################################################
+
+mass <- census_tract_data[census_tract_data$StateAbbr == "MA", ]
+subset_mass_binge <- mass[,c("TractFIPS","BINGE_CrudePrev" )]
+colnames(subset_mass_binge) <- c("GEOID", "value")
 
 
+eva <- tracts(state="MA", cb=T)
+
+# Convert the datum of the eva_sf object to WGS84
+eva_sf <- st_as_sf(eva)
+
+converted_sf <- st_transform(eva_sf, crs = "+proj=longlat +datum=WGS84")
+
+leaflet() %>%
+  addTiles() %>%
+  addPolygons(data = converted_sf, popup = ~NAME)
+
+eva %>%
+  leaflet() %>%
+  addTiles() %>%
+  addPolygons(popup=~NAME)
+
+eva_merged_binge <- geo_join(eva, subset_mass_binge, "GEOID", "GEOID")
+
+pal <- colorNumeric("Reds", domain=eva_merged_binge$value)
+
+eva_merged_binge <- subset(eva_merged_binge,(value))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+popup_suff_binge <- paste0("Crude Prevalence: ", as.character(eva_merged_binge$value),
+                           "\nTract: ", as.character(eva_merged_binge$TRACTCE))
 
 
 leaflet() %>%
   addProviderTiles("CartoDB.Positron") %>%
   setView(-71.0382679, 42.3489054, zoom = 7) %>%
-  addPolygons(data=eva_merged_arthritis,
-              fillColor =~pal(eva_merged_arthritis$value),
+  addPolygons(data=eva_merged_binge,
+              fillColor =~pal(eva_merged_binge$value),
               fillOpacity = .7,
               weight = .4,
               smoothFactor = .2,
               popup =~popup_suff_arth) %>%
-  addMarkers(data = mhm_filtered_mass, lng=mhm_filtered_mass$longitude, lat=mhm_filtered_mass$latitude, popup=mhm_filtered_mass$clinic_name) %>%
-  # addMarkers(lng=-71.07477, lat=42.29042, popup="The Family Van") %>%
+  addMarkers(data = mhm_filtered_mass, lng=mhm_filtered_mass$longitude, lat=mhm_filtered_mass$latitude, 
+             popup = paste0("<a href='", mhm_filtered_mass$website, "' target='_blank'>", mhm_filtered_mass$clinic_name, "</a>")) %>%
+  
   addLegend(pal=pal,
-            values=eva_merged_arthritis$value,
+            values=eva_merged_binge$value,
             position = "bottomright",
-            title="Crude Prevalence Arthritis")
-
-
-
-
-
-
-
-
-
-############### only family van in map
-
-leaflet() %>%
-  addProviderTiles("CartoDB.Positron") %>%
-  setView(-71.0382679, 42.3489054, zoom = 7) %>%
-  addPolygons(data=eva_merged_arthritis,
-              fillColor =~pal(eva_merged_arthritis$value),
-              fillOpacity = .7,
-              weight = .4,
-              smoothFactor = .2,
-              popup =~popup_suff_arth) %>%
-  addMarkers(lng=-71.07477, lat=42.29042, popup="The Family Van") %>%
-  addLegend(pal=pal,
-            values=eva_merged_arthritis$value,
-            position = "bottomright",
-            title="Crude Prevalence Arthritis")
-
-
-
-
-
-
-
-
-
-
+            title="Crude Prevalence Binge Drinking")
 
 
 
